990536eafa20dc94a0e8981b994e38f77643490f
Cao <cao.nguyenchi.25@gmail.com>
2026-07-14 20:38:33 +0700

    fix: clear old attachments when toggling shop categories in editReply

--- apps/bot/src/modules/economy/events/shopInteraction.ts
+++ apps/bot/src/modules/economy/events/shopInteraction.ts
@@ -88,6 +88,7 @@ export default class ShopInteractionEvent implements IEvent<'interactionCreate'>
 
     await interaction.editReply({
       files: [attachment],
+      attachments: [],
       components: [rowMenu, rowButtons]
     });
   }
